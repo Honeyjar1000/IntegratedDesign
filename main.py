478a1518cbae623bc70189054dc0ff0bbc3a6609
Honeyjar1000 <48975382+Honeyjar1000@users.noreply.github.com>
2025-08-19 18:06:58 +1000

    DELETE EVERYTHING

---
+++ main.py
@@ -0,0 +1,8 @@
+# scp -r D:\uni\ECE4179\project\IntegratedDesign\* tom@raspberrypi:~/Documents/
+
+def main():
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
--- raspberrypi_code/main.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print("Hello World")
-
-
-if __name__ == "__main__":
-    main()
-
-
-# scp -r "D:\uni\ECE4191\IntegratedDesign\raspberrypi_code" tom@raspberrypi:~/Documents/
