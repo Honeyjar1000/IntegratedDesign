851c633c02e6832ca17b7fe6b225a215afe58348
ahilaaa <ahila.krishnamurthi@gmail.com>
2025-08-20 18:13:50 +1000

    base server setup

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 # scp -r D:\uni\ECE4179\project\IntegratedDesign\* tom@raspberrypi:~/Documents/
+# scp -r /Users/ahila/Desktop/IntegratedDesign/* ahila@raspberrypi.local:/home/tom/Documents/ - AHILA
 
-def main():
-    print("Hello World")
+"""
+Flask automatically looks for:
 
+a templates/ folder → for HTML files used by render_template()
+a static/ folder → for CSS, JS, and images
+"""
+
+from flask import Flask, render_template
+import cv2
+from matplotlib import pyplot as plt
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=80, debug=True)
+
+
+# for any new route or URL, you just add @app.route(“/url-path”) followed by a Python function
+
+
+
+    
